0f934e3b368ed383e56f4086cfb5d2b86a30ec81
Ivelina Popova <iwelina.popova@gmail.com>
2016-02-09 10:51:11 +0200

    Initialize default MVC project

---
+++ Source/Web/MvcTemplate.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using HotelSystem.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var usersCount = db.Users.Count();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
