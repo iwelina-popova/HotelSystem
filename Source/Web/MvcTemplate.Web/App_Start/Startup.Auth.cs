e055e9cb05de86553f15f49b677529410ac99572
Ivelina Popova <iwelina.popova@gmail.com>
2016-02-09 12:46:47 +0200

    Remane ApplicationUser to User and extract it

--- Source/Web/MvcTemplate.Web/App_Start/Startup.Auth.cs
+++ Source/Web/MvcTemplate.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using HotelSystem.Web.Models;
+using HotelSystem.Models;
 
 namespace HotelSystem.Web
 {
@@ -30,7 +31,7 @@ namespace HotelSystem.Web
                 {
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, User>(
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
