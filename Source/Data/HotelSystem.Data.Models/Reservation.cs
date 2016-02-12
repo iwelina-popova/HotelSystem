1b79cf8340db60af1bb48fa559c788f87c908617
Ivelina Popova <iwelina.popova@gmail.com>
2016-02-12 10:40:58 +0200

    Add Bootstrap template

---
+++ Source/Data/HotelSystem.Data.Models/Reservation.cs
@@ -0,0 +1,32 @@
+﻿namespace HotelSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using HotelSystem.Data.Common.Models;
+
+    public class Reservation : BaseModel<int>
+    {
+        public DateTime ReservedOn { get; set; }
+
+        public int HotelRoomsId { get; set; }
+
+        public virtual HotelRooms HotelRooms { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Phone { get; set; }
+    }
+}
--- Source/Web/HotelSystem.Web/App_Start/BundleConfig.cs
+++ Source/Web/HotelSystem.Web/App_Start/BundleConfig.cs
@@ -7,24 +7,42 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //// bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            ////             "~/Scripts/jquery-{version}.js"));
+            ////
+            //// bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            ////             "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            ////             "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //// bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            ////           "~/Scripts/bootstrap.js",
+            ////           "~/Scripts/respond.js"));
+            ////
+            //// bundles.Add(new StyleBundle("~/Content/css").Include(
+            ////           "~/Content/bootstrap.css",
+            ////           "~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css/custom").Include(
+                "~/Content/custom/bootstrap.css",
+                "~/Content/custom/JFFormStyle-1.css",
+                "~/Content/custom/jquery-ui.css",
+                "~/Content/custom/owl.carousel.css",
+                "~/Content/custom/style.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/custom/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                "~/Scripts/custom/JFCore.js",
+                "~/Scripts/custom/JFForms.js",
+                "~/Scripts/custom/jquery-ui.js",
+                "~/Scripts/custom/jquery.flexisel.js",
+                "~/Scripts/custom/owl.carousel.js",
+                "~/Scripts/custom/responsiveslides.min.js"));
         }
     }
 }
