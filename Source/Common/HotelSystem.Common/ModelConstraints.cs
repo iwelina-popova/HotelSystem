92a6dd21bbc6fe73bc9a7baf387cca6b572f90ef
Ivelina Popova <iwelina.popova@gmail.com>
2016-02-22 10:53:47 +0200

    Add ModelConstraints and ModelValidationErrors constants

--- Source/Common/HotelSystem.Common/ModelConstraints.cs
+++ Source/Common/HotelSystem.Common/ModelConstraints.cs
@@ -6,6 +6,26 @@
 
         public const int NameMaxLength = 30;
 
-        public const int PasswordMinLength = 5;
+        public const int PasswordMinLength = 6;
+
+        public const int DescriptionMinLength = 50;
+
+        public const int DescriptionMaxLength = 2000;
+
+        public const int EmailMaxLength = 100;
+
+        public const int ContactInfoMaxLength = 20;
+
+        public const int FacebookMaxLength = 100;
+
+        public const int HotelMinStars = 1;
+
+        public const int HotelMaxStars = 5;
+
+        public const int LocationMaxLength = 100;
+
+        public const int RoomMinCapacity = 1;
+
+        public const int RoomMaxCapacity = 10;
     }
 }
--- Source/Common/HotelSystem.Common/ModelValidationErrors.cs
+++ Source/Common/HotelSystem.Common/ModelValidationErrors.cs
@@ -3,5 +3,11 @@
     public static class ModelValidationErrors
     {
         public const string InvalidModel = "Invalid model's data!";
+
+        public const string ValidationErrorForLength = "The {0} must no longer than {2} characters.";
+
+        public const string ValidationErrorForRange = "The {0} must be between {2} and {1} characters.";
+
+        public const string EditDeletedEntity = "Cannot edit deleted entity";
     }
 }
